7ac434557267a7c419a2e119666550a67b626c13
Ikenna <ikennaugwuh@gmail.com>
2016-07-21 08:01:18 -0400

    Added a new controller for the form and moved the addComment method within it in order to have access to the form controller's () method.

--- src/js/controllers/controller.js
+++ src/js/controllers/controller.js
@@ -1,20 +1,13 @@
 export default function Controller($scope, SERVER, $http) {
-    $scope.text = "Hello Add Controller";
     $scope.comments = [];
-    $scope.addComment = comment => {
-        $http.post(SERVER.URL, comment).then(res => {
-            $scope.comments.push(res.data);
-            $scope.comment = {};
-        });
-    };
     $scope.deleteComment = id => {
-        $http.delete(SERVER.URL + id).then(res => {
+        $http.delete(SERVER.URL + id).then(()=>{
             $scope.comments = $scope.comments.filter(c => c._id !== id);
         });
     }
     let init = ()=>{
-        $http.get(SERVER.URL).then(res => {
-            $scope.comments = res.data;
+        $http.get(SERVER.URL).then(resp => {
+            $scope.comments = resp.data;
         });
     };
     init();
--- src/js/main.js
+++ src/js/main.js
@@ -5,4 +5,13 @@ import SERVER from './server-constant'
 angular
    .module('app',[])
    .constant('SERVER', SERVER)
-   .controller('Controller', Controller);
+   .controller('Controller', Controller)
+   .controller('FormController', ['$scope', 'SERVER', '$http', function($scope, SERVER, $http){
+       $scope.addComment = comment => {
+           $http.post(SERVER.URL, comment).then(resp => {
+               $scope.comment = {};
+               $scope.Commenter.$setPristine();
+               $scope.comments.push(resp.data);
+           });
+       };
+   }]);
