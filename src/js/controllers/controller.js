33ba87a787e96428891dcff0ef6f336a3dece17f
Ikenna <ikennaugwuh@gmail.com>
2016-07-21 09:31:59 -0400

    Added a new way to validate the commenter name.

--- src/js/controllers/controller.js
+++ src/js/controllers/controller.js
@@ -1,9 +1,13 @@
 export default function Controller($scope, SERVER, $http) {
     $scope.comments = [];
+    $scope.error = {};
     $scope.deleteComment = id => {
         $http.delete(SERVER.URL + id).then(()=>{
             $scope.comments = $scope.comments.filter(c => c._id !== id);
         });
+    }
+    $scope.validate = name => {
+        $scope.error.name = name ? '' : "Required.";
     }
     let init = ()=>{
         $http.get(SERVER.URL).then(resp => {
